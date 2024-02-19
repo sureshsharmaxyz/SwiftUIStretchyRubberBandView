5f69377de145429464b14a35b3bd72a55ca24325
Suresh Sharma <sursha1993@gmail.com>
2024-02-19 10:34:02 +0100

    Initial Commit

---
+++ StretchyRubberBandEffect/StretchyRubberBandEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchyRubberBandEffectApp.swift
+//  StretchyRubberBandEffect
+//
+//  Created by Suresh Sharma on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct StretchyRubberBandEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
